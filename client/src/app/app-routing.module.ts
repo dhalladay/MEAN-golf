c2d823a4bdc874f8bdd09d268f46723b5bc28551
daveh <dave.halladay@outlook.com>
2022-06-10 23:16:20 -0600

    update routing to include rounds-list component and update app.component html template

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoundsListComponent } from './rounds-list/rounds-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rounds', pathMatch: 'full'},
+  {path: 'rounds', component: RoundsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
