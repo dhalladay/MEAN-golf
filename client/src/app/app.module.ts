9b59efbddbcb272e6e046589fe685ab1b7b07a92
daveh <dave.halladay@outlook.com>
2022-06-13 22:40:02 -0600

    create round-form component and import ReactiveFormsModule to app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,25 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RoundsListComponent } from './rounds-list/rounds-list.component';
+import { RoundFormComponent } from './round-form/round-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoundsListComponent
+    RoundsListComponent,
+    RoundFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/round-form/round-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-round-form',
+  template: `
+    <p>
+      round-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RoundFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
